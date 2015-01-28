07e82ffb392d409f6dfe82cf778139af5bff1bea
Prprpr <prusha@inbox.lv>
2015-01-28 20:32:56 +0200

    msg

--- testapp/app.js
+++
@@ -1,5 +0,0 @@
-console.log("Labdien!");
-setInterval(function(){
-	console.log("Sveiki");
-	},2000 )
-		console.log("studenti");
--- testapp/test.js
+++
@@ -1,9 +0,0 @@
-var net = require ('net');
-var server = net.createServer(
-function (socket) {
-socket.write('Hello');
-	socket.end('World');
-	})
-	server.listen(8080);
-	console.log('Server running');
-	
